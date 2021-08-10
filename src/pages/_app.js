d7c46b88617943827b959622029250074c674235
Dron2019 <mr.andrey@bigmir.net>
2021-08-10 15:28:55 +0300

    try to change loco scroll conainer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,50 @@
+/* eslint-disable no-unused-vars */
 import 'normalize.css';
 import '../styles/globals.css';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/dist/next-server/lib/head";
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { useRouter } from 'next/router';
 import theme from '../theme';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 
-
 function MyApp({ Component, pageProps }) {
+  // const router = useRouter();
+  // const [scrall, setScrall] = useState(null);
+  // useEffect((el) => {
+  //   console.log('I Start ONLY ONCE');
+  //   let scroll = null;
+  //   import('locomotive-scroll').then((locomotiveModule) => {
+
+  //     // eslint-disable-next-line new-cap
+  //     scroll = new locomotiveModule.default({
+  //       el: document.querySelector("#__next"),
+  //       smooth: true,
+  //       resetNativeScroll: true
+  //     });
+  //     React.createContext(scroll);
+  //     setTimeout(() => {
+  //       scroll.init();
+  //       setScrall(scroll);
+  //     }, 100);
+  //   });
+    
+  //   // return () => {
+  //   //   // scroll.destroy();
+  //   //   // document.querySelector('.c-scrollbar').remove()
+  //   // }
+  // }, []);
+
+  // useEffect(() => {
+  //   if (scrall !== null) {
+  //     scrall.start();
+  //     console.log(scrall);
+  //   }
+  // }, [router])
   useEffect(() => {
     let scroll = null;
     import('locomotive-scroll').then((locomotiveModule) => {
