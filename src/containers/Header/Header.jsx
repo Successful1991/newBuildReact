eb62ba10edbc841d157fa830938823209ac34e2b
Dron2019 <mr.andrey@bigmir.net>
2021-08-12 13:32:53 +0300

    add test material button in head

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState} from 'react';
 import { useRouter } from 'next/router';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
 import { useLocomotiveScroll } from 'react-locomotive-scroll';
+import Button from '@material-ui/core/Button';
+import { ReactComponent as BurgerIcon } from '../../../public/icons/burger.svg';
 import css from './Header.module.scss';
 // eslint-disable-next-line import/named
 import { Menu } from '../../components/UI/Menu/Menu';
 import { Langs } from '../../components/Langs/Langs';
 
+const useStyles = makeStyles((theme) => ({
+	button: {
+	  margin: theme.spacing(1),
+	},
+  }));
+
 export const Header = ({ translate }) => {
 	const { scroll } = useLocomotiveScroll();
+	const { t } = useTranslation();
 	const router = useRouter();
+	const classes = useStyles();
 	// const { scroll } = useContext(SmoothScrollContext);
 	const [isOnTop, isOnTopSet] = useState(true);
 	
@@ -25,8 +37,15 @@ export const Header = ({ translate }) => {
 	return (
 		<header className={headerClassList}>
 			шапка
+
 			{typeof translate === 'function' && translate('change-locale')}
 			<Langs router={router}/>
+			<Button
+				variant="contained"
+				color="secondary"
+				className={classes.button}
+				endIcon={<BurgerIcon style={{ width: '10px', height: '10px' }}/>}
+			>{t('change-locale')}</Button>
 			<Menu/>
 		</header>
 	);
