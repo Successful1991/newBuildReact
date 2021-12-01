045a1a6c28b1a7f3e0fbc6858b5d94cedab7e297
Dron2019 <mr.andrey@bigmir.net>
2021-12-01 10:17:51 +0200

    deploy 3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -45,7 +45,7 @@ export default function Home({ flats }) {
     <div className={styles.container} data-scroll-container id='app'>
       <Header translate={t}/>
       <section className={styles.main}>
-        <h1 data-scroll className={styles.title}>Starting template WITH DEPLOY 1245456453</h1>
+        <h1 data-scroll className={styles.title}>Starting template WITH DEPLOY1 1245456453</h1>
         <SelectTab />
         <Filter flats={flats} setFilteredFlats={setFilteredFlats}/>
         <div ref={refFilter} className={styles.cards} >
